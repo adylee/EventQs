87d42f163954bc79c9252655446f99343f7ab671
ad <ad@itmp.top>
2016-03-17 16:49:55 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/eventqs/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.administrator.eventqs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bn = (Button)findViewById(R.id.bn);
+        bn.setOnClickListener(new MyClickListener());
+    }
+    class MyClickListener implements View.OnClickListener
+    {
+        public void onClick(View v)
+        {
+            EditText txt = (EditText)findViewById(R.id.txt);
+            txt.setText("bn has been clicked!");
+        }
+    }
+}
